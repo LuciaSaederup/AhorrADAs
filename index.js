cb115185a697742abf9e307bd2deb0d2fe161399
Lucia Saederup <luciasaederup@gmail.com>
2021-11-18 12:14:14 -0300

    agregar nueva operacion en js

--- index.js
+++ index.js
@@ -21,3 +21,48 @@ verReportes.addEventListener("click", (e) => {
     seccionCategorias.classList.add("is-hidden");
     seccionReportes.classList.remove("is-hidden");
 });
+
+
+
+const botonNuevaOperacion = document.getElementById("boton-nueva-operacion");
+const seccionAgregarOperacion = document.getElementById("seccion-agregar-operacion");
+
+botonNuevaOperacion.addEventListener("click", (e) => {
+    seccionBalances.classList.add("is-hidden");
+    seccionAgregarOperacion.classList.remove("is-hidden");
+    seccionCategorias.classList.add("is-hidden");
+    seccionReportes.classList.add("is-hidden");
+
+})
+
+//---------------------------CREAR NUEVA OPERACION-------------------
+const descripcionNuevaOperacion = document.getElementById("input-descripcion-nueva-operacion");
+const montoNuevaOperacion = document.getElementById("monto-nueva-operacion");
+const tipoNuevaOperacion = document.getElementById("tipo-nueva-operacion");
+const categoriaNuevaOperacion = document.getElementById("categoria-nueva-operacion");
+const fechaNuevaOperacion = document.getElementById("fecha-nueva-operacion");
+const botonAgregarNuevaOperacion = document.getElementById("boton-agregar-operacion")
+const formularioOperacion = document.getElementById('formulario-operacion')
+const boxOperaciones = document.getElementById("operaciones-realizadas");
+
+// --------------------array de objetos(operaciones)----------------
+let arrayDeOperaciones = []
+let nuevoObjeto = {}
+
+console.log(arrayDeOperaciones)
+
+montoNuevaOperacion.onchange = () => {
+    console.log(montoNuevaOperacion.value)
+}
+tipoNuevaOperacion.onchange = (event) => {
+    console.log(tipoNuevaOperacion.value)
+}
+categoriaNuevaOperacion.onchange = (event) => {
+    console.log(categoriaNuevaOperacion.value)
+}
+descripcionNuevaOperacion.onchange = (event) => {
+    console.log(descripcionNuevaOperacion.value)
+}
+fechaNuevaOperacion.oninput = () => {
+    console.log(fechaNuevaOperacion.value)
+}
